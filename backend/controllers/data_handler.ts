15f40ae1f0b4c78dc4a6b1ac2d4dfdd0eb99281a
Jeevanjot Singh <40867747+VitalJeevanjot@users.noreply.github.com>
2022-08-12 21:06:33 +0530

    static submissions

--- backend/controllers/data_handler.ts
+++ backend/controllers/data_handler.ts
@@ -11,7 +11,8 @@ const Feedbacks = sequelize.define('Feedbacks', {
   feedback_id: DataTypes.STRING,
   feedback_ref: DataTypes.STRING
 });
-// Feedbacks.sync({ force: true });
+
+
 
 export const get_feedbacks: RequestHandler = async (req, res, next) => {
   try {
@@ -29,14 +30,7 @@ export const post_feedback: RequestHandler = async (req, res, next) => {
   console.log(req.body);
 
   try {
-    await Feedbacks.create({
-      user_posted_feedback: req.body.user_name,
-      the_feedback: req.body.feedback_body,
-      time_posted_feedback: Date.now().toString(),
-      feedback_upvotes: 0,
-      feedback_id: crypto.randomBytes(20).toString("hex"),
-      feedback_ref: req.body.feedback_id
-    });
+    await store(req.body.user_name, req.body.feedback_body, req.body.feedback_id)
     return res.status(200).send({saved: "done"});
 
   } catch(e) {
@@ -44,3 +38,76 @@ export const post_feedback: RequestHandler = async (req, res, next) => {
     return res.status(500).send();
   }
 }
+
+
+async function store(user_name: string, feedback_body: string, feedback_id: string) {
+  await Feedbacks.create({
+    user_posted_feedback: user_name,
+    the_feedback: feedback_body,
+    time_posted_feedback: Date.now().toString(),
+    feedback_upvotes: 0,
+    feedback_id: crypto.randomBytes(20).toString("hex"),
+    feedback_ref: feedback_id
+  });
+}
+
+// static submissions
+
+async function static_store(obj: object) {
+  await Feedbacks.create({
+    ...obj
+  });
+}
+
+async function initDB() {
+  await Feedbacks.sync({ force: true });
+  let ref1 = crypto.randomBytes(20).toString("hex")
+  const first_entries = [
+    {
+      user_posted_feedback: "Vital",
+      the_feedback: "I must say that this tutorial is too short to be considered knowledgeable.",
+      time_posted_feedback: Date.now().toString(),
+      feedback_upvotes: 49,
+      feedback_id: crypto.randomBytes(20).toString("hex"),
+      feedback_ref: "null"
+    },
+    {
+      user_posted_feedback: "Victor",
+      the_feedback: "You should respect the author, Short thing doesn't necessarily means less useful.",
+      time_posted_feedback:  (Date.now() + 1).toString(),
+      feedback_upvotes: 49,
+      feedback_id: crypto.randomBytes(20).toString("hex"),
+      feedback_ref: ref1
+    },
+    {
+      user_posted_feedback: "Vital",
+      the_feedback: "There is something I never told you @Victor. I am the Author.",
+      time_posted_feedback:  (Date.now() + 2).toString(),
+      feedback_upvotes: 49,
+      feedback_id: crypto.randomBytes(20).toString("hex"),
+      feedback_ref: ref1
+    },
+    {
+      user_posted_feedback: "Victor",
+      the_feedback: "Noooooooooooooooooo.",
+      time_posted_feedback:  (Date.now() + 3).toString(),
+      feedback_upvotes: 49,
+      feedback_id: crypto.randomBytes(20).toString("hex"),
+      feedback_ref: ref1
+    },
+    {
+      user_posted_feedback: "Jeevanjot",
+      the_feedback: "Nice TLDR, Easier to follow. I will add it into my newspaper (if i have one which is).",
+      time_posted_feedback:  (Date.now() + 4).toString(),
+      feedback_upvotes: 49,
+      feedback_id: crypto.randomBytes(20).toString("hex"),
+      feedback_ref: "null"
+    },
+  ]
+
+  for (let index = 0; index < first_entries.length; index++) {
+    const element = first_entries[index];
+    static_store(element)
+  }
+}
+initDB()
