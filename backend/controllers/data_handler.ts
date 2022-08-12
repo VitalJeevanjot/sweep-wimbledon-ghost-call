83c770a1bda04186fb8a7cd967fee2d77cb3ae02
Jeevanjot Singh <40867747+VitalJeevanjot@users.noreply.github.com>
2022-08-12 19:45:58 +0530

    Sequelize setup and data storage

--- backend/controllers/data_handler.ts
+++ backend/controllers/data_handler.ts
@@ -1,4 +1,46 @@
 import { RequestHandler } from 'express'
-export const get_feedbacks: RequestHandler = (req, res, next) => {
-  res.status(200).send("Yo HOOO!")
+import { Sequelize, Model, DataTypes } from 'sequelize';
+import crypto from 'crypto';
+
+const sequelize = new Sequelize('sqlite::memory:');
+const Feedbacks = sequelize.define('Feedbacks', {
+  user_posted_feedback: DataTypes.STRING,
+  the_feedback: DataTypes.STRING,
+  time_posted_feedback: DataTypes.STRING,
+  feedback_upvotes: DataTypes.INTEGER,
+  feedback_id: DataTypes.STRING,
+  feedback_ref: DataTypes.STRING
+});
+// Feedbacks.sync({ force: true });
+
+export const get_feedbacks: RequestHandler = async (req, res, next) => {
+  try {
+    const feedbacks = await Feedbacks.findAll();
+    return res.status(200).send({status: 'success', response: feedbacks});
+  } catch(e) {
+    console.log(e)
+    return res.status(500).send();
+
+  }
+}
+
+export const post_feedback: RequestHandler = async (req, res, next) => {
+
+  console.log(req.body);
+
+  try {
+    await Feedbacks.create({
+      user_posted_feedback: req.body.user_name,
+      the_feedback: req.body.feedback_body,
+      time_posted_feedback: Date.now().toString(),
+      feedback_upvotes: 0,
+      feedback_id: crypto.randomBytes(20).toString("hex"),
+      feedback_ref: req.body.feedback_id
+    });
+    return res.status(200).send({saved: "done"});
+
+  } catch(e) {
+    console.log(e)
+    return res.status(500).send();
+  }
 }
--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import * as handler from './controllers/data_handler'
+import cors from 'cors'
 const app = express()
+app.use(express.json())
+app.use(cors())
 const port = 3000
 
 app.get('/get_feedbacks', handler.get_feedbacks)
+app.post('/post_feedback', handler.post_feedback)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
