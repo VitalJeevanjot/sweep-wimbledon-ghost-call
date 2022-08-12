85bd4b6ca5b1ee3d0ec96eb904fe2173e18184d1
Jeevanjot Singh <40867747+VitalJeevanjot@users.noreply.github.com>
2022-08-12 13:26:28 +0530

    centering the discussion layout

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
   $(document).ready(function(){
     $('.materialboxed').materialbox();
-    var git_post = "https://gist.githubusercontent.com/VitalJeevanjot/43647078efd970be3e083afe3470551a/raw/db3422ed851c3da08eb0a1a67dec8a9305c4ecbf/uniblogpost.md"
+    var git_post = "https://gist.githubusercontent.com/VitalJeevanjot/43647078efd970be3e083afe3470551a/raw/30fc8217c905280350c63067d258a54a331c0bab/uniblogpost.md"
     axios.get(git_post)
     .then(function (response) {
       document.getElementById('content').innerHTML = 
