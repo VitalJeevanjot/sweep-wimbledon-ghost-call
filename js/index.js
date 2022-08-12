52535d2c07ea872e9d484b82cab00f2ce2b5ceac
Jeevanjot Singh <40867747+VitalJeevanjot@users.noreply.github.com>
2022-08-12 13:13:14 +0530

    Markdown post integration and basic discussion format

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
   $(document).ready(function(){
     $('.materialboxed').materialbox();
+    var git_post = "https://gist.githubusercontent.com/VitalJeevanjot/43647078efd970be3e083afe3470551a/raw/db3422ed851c3da08eb0a1a67dec8a9305c4ecbf/uniblogpost.md"
+    axios.get(git_post)
+    .then(function (response) {
+      document.getElementById('content').innerHTML = 
+      marked.parse(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
   });
